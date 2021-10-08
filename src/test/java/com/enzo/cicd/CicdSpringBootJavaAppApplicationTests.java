e8f58e73e029cceb63987fbba89fc9ad99c72fbb
django123 <edougajean@gmail.com>
2021-10-08 16:56:46 +0100

    First upload

---
+++ src/test/java/com/enzo/cicd/CicdSpringBootJavaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enzo.cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdSpringBootJavaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
